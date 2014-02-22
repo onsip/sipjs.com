614736621b29f931f003d312d19a522527815b6b
Phil Hess <phess@andculture.com>
2014-02-22 09:34:57 -0500

    Submission of AndCulture Changes

--- static/shared/js/documentation.js
+++ static/shared/js/documentation.js
@@ -1,111 +1,59 @@
 // Init sidebar
 $(function() {
-  var activeItem,
-      helpList = $('#js-sidebar .js-topic'),
-      firstOccurance = true,
-      styleTOC = function() {
-        var pathRegEx = /\/\/[^\/]+(\/.+)/g,
-            docUrl = pathRegEx.exec(window.location.toString())
-        if (docUrl){
-          $('#js-sidebar .js-topic a').each(function(){
-            if ($(this).parent('li').hasClass('disable'))
-              $(this).parent('li').removeClass('disable')
 
-            var url = $(this).attr('href').toString()
-            var cleanDocUrl = docUrl[1]
-            if(url.indexOf(cleanDocUrl) >= 0 && url.length == cleanDocUrl.length){
-              $(this).parent('li').addClass('disable')
-              var parentTopic = $(this).parentsUntil('div.sidebar-module > ul').last()
-              parentTopic.addClass('js-current')
-              parentTopic.find('.js-expand-btn').toggleClass('collapsed expanded')
-            }
-          });
-        }
-      }
+  var parents = $('#sideNav li.parent');
+  var children = parents.children('ul');
 
-  // bind every href with a hash; take a look at v3/search/ for example
-  $('#js-sidebar .js-accordion-list .js-topic a[href*=#]').bind("click", function(e) {
-    if (window.location.toString().indexOf($(e.target).attr('href')) == -1)
-      setTimeout(styleTOC, 0); // trigger the window.location change, then stylize
+  // Hide submenus
+  children.addClass('hidden');
+
+  $('#sideNav a').click(function(e){
+    e.stopPropagation();
   });
 
-  // hide list items at startup
-  if($('body.api') && window.location){
-    styleTOC();
-  }
-
-  $('#js-sidebar .js-topic').each(function(){
-    if(($(this).find('.disable').length == 0 || firstOccurance == false) &&
-    $(this).hasClass('js-current') != true){
-      $(this).find('.js-guides').children().hide()
-    } else {
-      activeItem = $(this).index()
-      firstOccurance = false
-    }
-  })
-
-  // Toggle style list. Expanded items stay
-  // expanded when new items are clicked.
-  $('#js-sidebar .js-toggle-list .js-expand-btn').click(function(){
-    var clickedTopic = $(this).parents('.js-topic'),
-        topicGuides  = clickedTopic.find('.js-guides li')
-    $(this).toggleClass('collapsed expanded')
-    topicGuides.slideToggle(100)
-    return false
-  })
-
-  // Accordion style list. Expanded items
-  // collapse when new items are clicked.
-  $('#js-sidebar .js-accordion-list .js-topic h3 a').click(function(){
-    var clickedTopic = $(this).parents('.js-topic'),
-        topicGuides = clickedTopic.find('.js-guides li')
-
-    if(activeItem != clickedTopic.index()){
-      if(helpList.eq(activeItem)){
-        helpList.eq(activeItem).find('.js-guides li').slideToggle(100)
-      }
-      activeItem = clickedTopic.index()
-      topicGuides.slideToggle(100)
-    } else {
-      activeItem = undefined
-      topicGuides.slideToggle(100)
-    }
-
-    return false
-  })
-
-  $('.help-search .search-box').focus(function(){
-    $(this).css('background-position','0px -25px')
-  })
-
-  $('.help-search .search-box').focusout(function(){
-    if($(this).val() == ''){
-      $(this).css('background-position','0px 0px')
-    }
-  })
-
-  // Dynamic year for footer copyright
-  var currentYear = (new Date).getFullYear();
-  $("#year").text( (new Date).getFullYear() );
-
-  // Grab API status
-  $.getJSON('https://status.onsip.com/api/status.json?callback=?', function(data) {
-    if(data) {
-      var link = $("<a>")
-        .attr("href", "https://status.onsip.com")
-        .addClass(data.status)
-        .attr("title", "API Status: " + data.status + ". Click for details.")
-        .text("API Status: " + data.status);
-      $('.api-status').html(link);
-    }
+  parents.each(function(index) {
+    $(this).click(function(){
+      $(this).find('ul').toggleClass('hidden');
+      $(this).toggleClass('active');
+    })
   });
 
-  // Add link anchors for headers with IDs
-  $(".content h1, .content h2, .content h3, .content h4").each(function(e){
-    var id = $(this).attr("id");
-    if (!id) return;
+  // Open submenus and set active state on page load where needed
+  openMenus(null);
 
-    $(this).prepend("<a class='header-anchor' href='#" + id + "'></a>");
+  $('#sideNav a').click(function(e){
+    var newLink = $(this).attr('href');
+    
+
+    openMenus(newLink);
   });
 
 });
+
+function openMenus(_newLink) {
+  var newLink = _newLink;
+  var currentPage = String(window.location.pathname + window.location.hash);
+
+  if (newLink !== null) {
+    currentPage = newLink;
+  }
+  console.log('Opening for' + currentPage);
+
+  $('#sideNav li').removeClass('active');
+
+  $('#sideNav a').each(function(){
+    var linkRef = $(this).attr('href');
+    if (currentPage == linkRef) {
+      $(this).parents('li').addClass('active');
+      
+      if ($(this).parents('li').hasClass('parent')) {
+        $(this).parents('ul').removeClass('hidden');
+        $(this).parents('li.parent').addClass('active');
+      }
+
+      if ($(this).hasClass('parent')) {
+        $(this).parents('li').children('ul').removeClass('hidden');
+      }
+    }
+  });
+}
