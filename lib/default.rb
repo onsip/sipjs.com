5f46834f8e3fc35a4693dfeddb9348aa1fa286ef
Joseph Frazier <joseph@onsip.com>
2014-04-14 16:11:12 -0400

    No CodeRay line_number_anchors\!\!\! see developer.onsip.com commit 85e01e53b8ac468e2a786346df3a9b9dbf4f1026

--- lib/default.rb
+++ lib/default.rb
@@ -2,3 +2,7 @@
 # before nanoc starts compiling.
 include Nanoc::Helpers::Rendering
 include Nanoc3::Helpers::XMLSitemap
+
+require('coderay');
+
+CodeRay::Encoders::HTML::DEFAULT_OPTIONS[:line_number_anchors] = false
