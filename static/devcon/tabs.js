134cd0b7a2d9e1e0906f44c3ea7c126e915e0fcc
Jason Salsiccia <jason@junctionnetworks.com>
2014-07-07 16:45:44 -0400

    add container for code tabs

---
+++ static/devcon/tabs.js
@@ -0,0 +1,20 @@
+(function () {
+    var tab_links = document.getElementById('codebox').getElementsByTagName('a');
+    for(var i = 0; i < tab_links.length; i++) {
+        tab_links[i].addEventListener('click', function(e) {
+            onTabChange(e.target.getAttribute("data-tab"));
+        });
+    }
+
+    function onTabChange(tab_num) {
+        var tabs = document.getElementById('code-tabs').getElementsByTagName('div');
+        for (var i = 0; i < tabs.length; i++) {
+            tabs[i].style.display = (tabs[i].getAttribute("data-tab") == tab_num) ? 'block' : 'none';
+        }
+
+        var tab_links = document.getElementById('codelist').getElementsByTagName('a');
+        for(var i = 0; i < tab_links.length; i++) {
+            tab_links[i].parentNode.className = (tab_links[i].dataset.tab == tab_num) ? 'active' : '';
+        }
+    }
+})();
