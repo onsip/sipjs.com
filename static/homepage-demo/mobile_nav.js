aae00c76d5b7478768fd40918a9c0727548cd00d
Dylan Mikus <dylan@onsip.com>
2014-08-20 14:32:13 -0400

    The pop down drawer now works

--- static/homepage-demo/mobile_nav.js
+++ static/homepage-demo/mobile_nav.js
@@ -5,37 +5,41 @@ var headerExpandHeight = '38vw';
 var shrunkNavBottom = '0';
 var expandNavBottom = '7.5%';
 var menuOpen = false;
+var showDelay = 150;
 var hideDelay = 400;
 var hideFunc = (function () {
-    if (!menuOpen) {
+    if (!menuOpen)
         $('#full-nav').css('visibility', 'hidden');
-    }
 });
+var showFunc = (function () {
+    if (menuOpen)
+        $('#full-nav').css('visibility', 'visible');
+})
 
 $('#mobile-menu').click(function (e) {
     if (!menuOpen) {
+        menuOpen = true;
         $('#siteHeader').css('height', headerExpandHeight);
-        $('#full-nav').css('visibility', 'visible');
         $('#full-nav').css('opacity', '1');
         $('#full-nav').css('bottom', expandNavBottom);
-        menuOpen = true;
+        setTimeout(showFunc, showDelay);
     } else {
+        menuOpen = false;
         $('#siteHeader').css('height', headerShrunkHeight);
         $('#full-nav').css('opacity', '0');
         $('#full-nav').css('bottom', shrunkNavBottom);
         setTimeout(hideFunc, hideDelay);
-        menuOpen = false;
     }
 });
 
 document.addEventListener('click', function (event) {
     // Tests if we clicked outside of the menu button
-    if ($(event.target).closest('#mobile-menu').length === 0) {
+    if ($(event.target).closest('#siteHeader').length === 0) {
+        menuOpen = false;
         $('#siteHeader').css('height', headerShrunkHeight);
         $('#full-nav').css('opacity', '0');
         $('#full-nav').css('bottom', shrunkNavBottom);
         setTimeout(hideFunc, hideDelay);
-        menuOpen = false;
     }
 });
 
