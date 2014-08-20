7a71f83ca90be4ece2a847e3d754177aba45ada9
Dylan Mikus <dylan@onsip.com>
2014-08-20 13:53:45 -0400

    Added divider between header and header drawer on mobile

--- static/homepage-demo/mobile_nav.js
+++ static/homepage-demo/mobile_nav.js
@@ -4,37 +4,38 @@ var headerShrunkHeight = '19vw';
 var headerExpandHeight = '38vw';
 var shrunkNavBottom = '0';
 var expandNavBottom = '7.5%';
+var menuOpen = false;
 var hideDelay = 400;
 var hideFunc = (function () {
-    $('#full-nav').css('visibility', 'hidden');
+    if (!menuOpen) {
+        $('#full-nav').css('visibility', 'hidden');
+    }
 });
-
-var menuOpen = false;
 
 $('#mobile-menu').click(function (e) {
     if (!menuOpen) {
-        menuOpen = true;
         $('#siteHeader').css('height', headerExpandHeight);
         $('#full-nav').css('visibility', 'visible');
         $('#full-nav').css('opacity', '1');
         $('#full-nav').css('bottom', expandNavBottom);
+        menuOpen = true;
     } else {
-        menuOpen = false;
         $('#siteHeader').css('height', headerShrunkHeight);
         $('#full-nav').css('opacity', '0');
         $('#full-nav').css('bottom', shrunkNavBottom);
         setTimeout(hideFunc, hideDelay);
+        menuOpen = false;
     }
 });
 
 document.addEventListener('click', function (event) {
     // Tests if we clicked outside of the menu button
     if ($(event.target).closest('#mobile-menu').length === 0) {
-        menuOpen = false;
         $('#siteHeader').css('height', headerShrunkHeight);
         $('#full-nav').css('opacity', '0');
         $('#full-nav').css('bottom', shrunkNavBottom);
         setTimeout(hideFunc, hideDelay);
+        menuOpen = false;
     }
 });
 
