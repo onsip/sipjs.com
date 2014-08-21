4e9f497dd1ce06fd2df04149ae22cf7cac43639d
Dylan Mikus <dylan@onsip.com>
2014-08-21 18:35:49 -0400

    Changed the popdown menu for mobile to be a legit drawer instead of just expanding the header height

--- static/homepage/mobile_nav.js
+++ static/homepage/mobile_nav.js
@@ -1,39 +1,27 @@
 (function () {
 
-/* For desktop layout */
-var headerDesktopHeight = '120px';
-
-var headerShrunkHeight = '19vw';
-var headerExpandHeight = '48vw';
-var shrunkNavBottom = '0';
-var expandNavBottom = '7.5%';
-expandNavBottom = '8vw';
+var drawerShrunkHeight = '0';
+var drawerExpandHeight = '20vw';
 var menuOpen = false;
 var showDelay = 150;
 var hideDelay = 400;
 var hideFunc = (function () {
     if (!menuOpen)
-        $('#full-nav').css('visibility', 'hidden');
+        $('#drawer-nav').css('visibility', 'hidden');
 });
 var showFunc = (function () {
     if (menuOpen)
-        $('#full-nav').css('visibility', 'visible');
+        $('#drawer-nav').css('visibility', 'visible');
 })
 
 $('#mobile-menu').click(function (e) {
     if (!menuOpen) {
         menuOpen = true;
-        $('#siteHeader').css('height', headerExpandHeight);
-        $('#full-nav').css('opacity', '1');
-        $('#full-nav').css('bottom', expandNavBottom);
-        $('#expand-divider').css('visibility', 'visible');
+        $('#drawer-nav').css('height', drawerExpandHeight);
         setTimeout(showFunc, showDelay);
     } else {
         menuOpen = false;
-        $('#siteHeader').css('height', headerShrunkHeight);
-        $('#full-nav').css('opacity', '0');
-        $('#full-nav').css('bottom', shrunkNavBottom);
-        $('#expand-divider').css('visibility', 'hidden');
+        $('#drawer-nav').css('height', drawerShrunkHeight);
         setTimeout(hideFunc, hideDelay);
     }
 });
@@ -41,12 +29,10 @@ $('#mobile-menu').click(function (e) {
 document.addEventListener('click', function (event) {
     // Tests if we clicked outside of the menu button
     if ($('#mobile-nav').css('display') !== 'none'
-        && $(event.target).closest('#mobile-menu').length === 0) {
+        && $(event.target).closest('#mobile-menu').length === 0
+        && $(event.target).closest('#drawer-nav').length === 0) {
         menuOpen = false;
-        $('#siteHeader').css('height', headerShrunkHeight);
-        $('#full-nav').css('opacity', '0');
-        $('#full-nav').css('bottom', shrunkNavBottom);
-        $('#expand-divider').css('visibility', 'hidden');
+        $('#drawer-nav').css('height', drawerShrunkHeight);
         setTimeout(hideFunc, hideDelay);
     }
 });
@@ -54,8 +40,9 @@ document.addEventListener('click', function (event) {
 
 $(window).resize(function (event) {
     if (window.innerWidth > 700) {
-        $('#siteHeader').removeAttr('style');
-        $('#full-nav').removeAttr('style');
+        menuOpen = false;
+        $('#drawer-nav').removeAttr('style');
+        $('#drawer-list').removeAttr('style');
     }
 });
 
