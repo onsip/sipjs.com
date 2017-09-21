e9695f522f1b1458bf88f4ca0f9bec4466a675da
Eric Green <eric.green@onsip.com>
2017-09-21 10:49:57 -0400

    Remove old devcon pages

--- static/devcon/alice.js
+++
@@ -1,6 +0,0 @@
-(function (window, document, undefined) {
-  window.caller = 'Alice';
-  window.uri = 'alice.' + window.token + '@devcon5.onsip.com';
-  window.callee = 'Bob';
-  window.target = 'bob.' + window.token + '@devcon5.onsip.com';
-}) (window, document);
--- static/devcon/bob.js
+++
@@ -1,6 +0,0 @@
-(function (window, document, undefined) {
-  window.caller = 'Bob';
-  window.uri = 'bob.' + window.token + '@devcon5.onsip.com';
-  window.callee = 'Alice';
-  window.target = 'alice.' + window.token + '@devcon5.onsip.com';
-}) (window, document);
